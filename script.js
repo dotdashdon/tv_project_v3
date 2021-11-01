4732a9e398133fe5d4575dbdf4596518f5b20cea
donna <dotdashhenry@gmail.com>
2021-11-01 12:56:43 +0000

    js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+
+
+
+const API_URL = 'https://api.tvmaze.com/shows/82/episodes'
+const IMG_PATH = 'https://api.tvmaze.com/shows/1/images'
+const SEARCH_URL = 'https://api.tvmaze.com//search/shows?&query=""'
+
+
+//
+getEpisodes(API_URL)
+
+
+async function getEpisodes(url) {
+   const res = await fetch(url)
+   const data = await res.json()
+   
+   console.log(data.results)
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
-window.onload = setup;
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
+
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// }
+
+// window.onload = setup;
