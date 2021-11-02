febd0c4cea4d656aecc66adc0af3107cb110cba4
donna <dotdashhenry@gmail.com>
2021-11-02 12:33:45 +0000

    js search

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-const API_URL = 'https://api.tvmaze.com/shows/82/episodes'
-const IMG_PATH = 'https://api.tvmaze.com/shows/1/images'
-const SEARCH_URL = 'https://api.tvmaze.com//search/shows?&query='
 
-const form =document.getElementById('form')
-const search = document.getElementById('')
+ const API_URL = 'https://api.tvmaze.com/shows/82/episodes'
+// const SEARCH_API = 'https://api.tvmaze.com//search/shows?&q=SEARCH_QUERY'
 
-getEpisodes(API_URL)
+fetch(API_URL)
+.then((response)=>{
+  console.log('we got a response');
+  console.log(response);
+  return response.json()
+})
+.then((jsonData) => {
+  const results = jsonData.map(element =>element.name);
+  displaySearchResults(results);
+});
 
 
-async function getEpisodes(url) {
-   const res = await fetch(url)
-   const data = await res.json()
-   
-   console.log(data.results)
+
+function searchShow(query) {
+     const SEARCH_API =`https://api.tvmaze.com//search/shows?&q=${query}`;
+     fetch(SEARCH_API)
+     .then((response)=>{
+           return response.json()
+   })
+     .then((jsonData) => {
+     console.log(jsonData);
+});
+
 }
 
-form.addEventListener ('submit', (e) => {
-  e.preventDefault()
-})
+
+function displaySearchResults (results) {
+  const list = document.getElementById('resultsList');
+  list.innerHTML = "";
+  results.forEach(result => {
+    const element = document.createElement('li');
+    element.innerText =result;
+    list.appendChild(element);
+  });
+}
+
+
+window.onload = () => {
+  const searchElement= document.getElementById('search');
+   searchElement.oninput =(event) =>{
+  searchShow(searchElement.value);
+   }
+  }
+
+
+
+
+
+
+
+
+// const API_URL = 'https://api.tvmaze.com/shows/82/episodes'
+// const IMG_PATH = 'https://api.tvmaze.com/shows/1/images'
+// const SEARCH_URL = 'https://api.tvmaze.com//search/shows?&q=SEARCH_QUERY'
+
+// const form =document.getElementById('form')
+// const search = document.getElementById('')
+
+// getEpisodes(API_URL)
+
+
+// async function getEpisodes(url) {
+//    const res = await fetch(url)
+//    const data = await res.json()
+   
+//    console.log(data.results)
+// }
+
+// form.addEventListener ('submit', (e) => {
+//   e.preventDefault()
+// })
 // function setup() {
 //   const allEpisodes = getAllEpisodes();
 //   makePageForEpisodes(allEpisodes);
