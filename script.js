f09edb51a174797a8aecc68ffaeb47e37ea1cde9
donna <dotdashhenry@gmail.com>
2021-11-11 11:08:37 +0000

    episodes list

--- script.js
+++ script.js
@@ -1,43 +1,85 @@
 
  const API_URL = 'https://api.tvmaze.com/shows/82/episodes'; //returns object with episode data
  const SEARCH_API = 'https://api.tvmaze.com//search/shows?&q=SEARCH_QUERY';//search of object 
-
+ 
  let allEpisodes =[];  //to store array of episodes fetched
 
 const form =document.getElementById('form');
 const search = document.getElementById('search');
 const main = document.getElementById('main');
-const selectEp = document.getElementById('')
+const selectEp = document.getElementById('');
+const selectShow = document.getElementById('');
 
 
 function setup() {
-  getEpisodes(API_URL);
+  getEpisodes(API_URL); //calls the getEpisodes function passing in API URL 
+  const select =  document.getElementById('dropDownEpisodes'); //grabbing select element 
+  select.addEventListener ('change', handleSelect);
+}
+
+function handleSelect(event) {  //called every time something changes in select 
+  const value = event.target.value; //capturing value from event. (value of option clicked on - id or empty string) 
+  console.log(value);
+  if (value){
+    const filteredEps = allEpisodes.filter ((episode) => {
+       return episode.id == value;
+    })
+    showEpisodes(filteredEps);
+  } else {
+    showEpisodes(allEpisodes);
+  }
+}
+
+//using fetch() to make GET request from URL. 
+async function getEpisodes(url) {
+   const res = await fetch(url);  //instead of doing .then 
+   allEpisodes = await res.json();    //uses .json() method to convert response to JSON
+  populateEpisodeDropdown(allEpisodes);
+   showEpisodes(allEpisodes);  //runs showEpisodes 
+}
+
+function populateEpisodeDropdown(episodes) {
+    const select =  document.getElementById('dropDownEpisodes');
+    select.innerHTML = '<option value="" >All Episodes</option>'; //putting select back to have one option 
+    episodes.forEach((episode) =>{
+      const {name,season, number,id} = episode;
+      const option = document.createElement('option');
+      option.value = id;
+      option.innerHTML = `S${season               //use this to create another function for drop down
+      .toString()
+      .padStart(2, "0")}: E${number.toString().padStart(2, "0")} ${name}`;
+      select.appendChild(option);
+    })
+
 }
 
 
-async function getEpisodes(url) {
-   const res = await fetch(url);  //instead of doing .then 
-   allEpisodes = await res.json();
+ function getShows(shows) {
 
-   showEpisodes(allEpisodes);
+ } 
+
+
+function populateShowDropdown(shows){
+  const select = document.getElementById('dropDownShow');
+  select.innerHTML = '<option value="" >All Shows</option>';
+
 }
 
 function showEpisodes(episodes) {
  main.innerHTML ='' //to stop episodes being added on to the bottom 
-
- episodes.forEach((episode )=> {
-	 const episodeEl = createEpisodeCard(episode);
+ episodes.forEach((episode )=> {    //looping through episode array & creates new element for each episode?
+	 const episodeEl = createEpisodeCard(episode); //creates new element 
 	 main.appendChild(episodeEl);													 
  })     												
 
 }
 
 function createEpisodeCard (episode){
-  const {name,season, number,image , summary} = episode;
+  const {name,season, number,image , summary} = episode; //destructuring 
 	const episodeEl = document.createElement('div');
 	episodeEl.classList.add('episode')
 
-	episodeEl.innerHTML =`
+	episodeEl.innerHTML =`   
       <img src="${image.medium}" alt="${name}">
       <div class="episode-info">
         <h3>${name}</h3>
@@ -54,14 +96,11 @@ function createEpisodeCard (episode){
 }
 
 
-//NEED TO CREATE FUNCTION FOR DROPDOWN SHOW - this function will pull required data from api and append to 
-//select option. Then need a an event listener of 'change' to make it work. Possibly use create card but use Remove? 
-// dropDownEpisodes
 
-// function selectShow(show) {
-//    const {season, name, number} = season;
-//    const seasonEl =document.createElement('form'); // should thisbe form , select or div?
-//    seasonEl.classList.add('season')
+// function populateSelectShow(show) {
+//    const { name, id} = show;
+//    const seasonEl =document.createElement(''); // should thisbe form , select or div?
+//    seasonEl.classList.add('season') //adding class 
 
 //    seasonEl.innerHTML =`
 //    <label for="dropDownSeason"></label>
@@ -77,10 +116,6 @@ function createEpisodeCard (episode){
 //    `
 //    return seasonEl;
 // }
-
-// selectShow.addEventListener("change", e => {
-
-//})
 
 
 
